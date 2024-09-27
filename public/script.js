cc0271f12d465992d5ac9780931e55942dd75d78
TheoMa76 <tmaerten@normandiewebschool.fr>
2024-09-27 14:36:06 +0200

    normally it works

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ document.getElementById('sla-form').addEventListener('submit', async function (e
     const sla = document.getElementById('sla').value;
     const period = document.getElementById('period').value;
   
-    const response = await fetch('/netlify/functions/sla.mjs', {
+    const response = await fetch('https://exercice6.netlify.app/.netlify/functions/sla', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
