80da34c10851c08febdff255a07eac690ec82e2f
TheoMa76 <tmaerten@normandiewebschool.fr>
2024-09-27 14:30:22 +0200

    trying to fix 404 errors

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ document.getElementById('sla-form').addEventListener('submit', async function (e
     const sla = document.getElementById('sla').value;
     const period = document.getElementById('period').value;
   
-    const response = await fetch('/.netlify/functions/sla.js', {
+    const response = await fetch('/.netlify/functions/sla', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
